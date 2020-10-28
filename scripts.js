efec8ecd2e69edf4b0dcd9b90424cdbed0cb3825
Isabell Betar <isabellbetar@Isabells-MacBook-Pro.local>
2020-10-28 12:09:23 +0100

    add content in css and js

--- scripts.js
+++ scripts.js
@@ -6,15 +6,40 @@ function toggleText() {
         text.style.display = "none";
     }
 }
+// state
 let currentScene = 0
 
 const scenes = [
      {
-         description: "Du har gott villse mitt ut i skogen. du kommer inte ihåg var du kom från. Du står mitt på en väg och funderar åt vilket håll du ska för att komma hem? ska ti ta Höger elle venster?"
-         choices: ["höger", "vänster"]
+         description: "Du har gott villse mitt ut i skogen. du kommer inte ihåg var du kom från. Du står mitt på en väg och funderar åt vilket håll du ska för att komma hem? ska ti ta Höger elle venster?",
+         choices: ["höger", "vänster"],
+         nextScene: [2,1]
 
-     }
-]
+     },
+     {
+        description: "du går och går. nur är du hungrig, thurstig och fryser,helt plöstligt ser du en liten stuga gömd bakom träden. på dörren står de välkommen maten är serverad.vad gör du? knackar på eller springer därifrån?",
+        choices: ["knackar på", "springer"],
+        nextScene: [2,1]
+
+    }
+];
+// run aplikation
+window.onload = presentScene
+
+// Aktions
+
+function presentScene() {
+    const answer = prompt(scenes[currentScene].description);
+
+}
+function handelUserChoice(answer) {
+    console.log(answer)
+
+    if (answer === scenes[currentScene].choices[0]){
+        currentScene = scenes [currentScene].nextScene[0]
+    }
+    presentScene()
+}
 
       
 
