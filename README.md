845f72ae5703198f7060e2d3acd1a6d9b3ce8c05
Isabell Betar <isabellbetar@Isabells-MacBook-Pro.local>
2020-10-29 14:02:59 +0100

    fix in js

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# interaktiv-upplevelse
+# interaktiv-upplevelse 
+
+## Interaktiv Upplebelse 
+
+I den här uppgiften har jag skapat en värld där använaren kan intarigera med världen ginom att välja mellan olika val.
+
+ [Demo](https://isabell88.github.io/interaktiv-upplevelse/)
--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+
+
 // function toggleText() {
 //     var text = document.getElementById("story1"); 
 //      if (text.style.display === "none"){
@@ -7,74 +9,79 @@
 //     }
 // }
 
-function startFunction() {
-
 // the state 
 
 let currentScene = 0
 
 const scenes = [
-     {
-         description: "Du har gått vilse mitt ut i skogen. du kommer inte ihåg var du kom från. Du står mitt på en väg och funderar åt vilket håll du ska för att komma hem? ska du ta höger elle venster?",
-         choices: ["höger", "venster"],
-         nextScene: [2, 5]
+    {
+        description: "Du har gått vilse mitt ut i skogen. du kommer inte ihåg var du kom från. Du står mitt på en väg och funderar åt vilket håll du ska för att komma hem? ska du ta höger elle venster?",
+        choices: ["höger", "vänster"],
+        nextScene: [1, 4]
 
-     },
-     {
+    },
+    {
         description: "Du går och går. Nu är du hungrig, törstig och du fryser, helt plöstligt ser du en liten stuga gömd bakom träden. på dörren står de välkommen! vad gör du? knackar på eller springer?",
         choices: ["knackar på", "springer"],
-        nextScene: [3, 5]
+        nextScene: [2, 4]
     },
 
     {
         description: "En gammal kvinna öppnar dörren. Hon har en lång näsa med en stor finne på. Det står två tallikar serverade med mat på bordet. kvinnan drar ut stolen för dig och ber dig att sitta. vad gör du? sitter eller springer?",
         choices: ["sitter", "springer"],
-        nextScene: [4, 5]
+        nextScene: [3, 4]
     },
     {
         description: "Du sätter dig på stolen och börjar äta, men stolen viker sig ner under golvet och du blir inlåst i en källare. där nere finns inget förutom en stol och ett bord, du märker att de finns en ventelations öppning högt upp på väggen. vad gör du nu? sätter du stolen på bordet och kryper ur ventelationen? ja eller nej",
         choices: ["ja", "nej"],
-        nextScene: [5, 0]
+        nextScene: [4,]
     },
     {
-        description: "Du springer raka vägen därifrån de snabbaste du bara kan och snart är du hemma safe and sound",
-        
+        description: "Du springer raka vägen därifrån de snabbaste du bara kan och snart är du hemma safe and sound. VILL DU SPELA IGEN",
+        choices: [],
+        nextScene: []
     }
 
 ];
-// run aplikation
-window.onload = presentScene
 
-// Aktions
+// Define Actions
 
 function presentScene() {
     const answer = prompt(scenes[currentScene].description);
-
+    handelUserChoice(answer)
 }
+
 function handelUserChoice(answer) {
     console.log(answer)
+    
+    
 
-    if (answer === scenes[currentScene].choices[0]){
-        currentScene = scenes [currentScene].nextScene[1]
+    if (answer === scenes[currentScene].choices[0]) {
+        currentScene = scenes[currentScene].nextScene[0]
     }
-    if (answer === scenes[currentScene].choices[1]){
-        currentScene = scenes [currentScene].nextScene[2]
+    if (answer === scenes[currentScene].choices[1]) {
+        currentScene = scenes[currentScene].nextScene[1]
     }
-    if (answer === scenes[currentScene].choices[2]){
-        currentScene = scenes [currentScene].nextScene[3]
+    if (answer === scenes[currentScene].choices[2]) {
+        currentScene = scenes[currentScene].nextScene[2]
     }
-    if (answer === scenes[currentScene].choices[3]){
-        currentScene = scenes [currentScene].nextScene[4]
+    if (answer === scenes[currentScene].choices[3]) {
+        currentScene = scenes[currentScene].nextScene[3]
     }
-    if (answer === scenes[currentScene].choices[4]){
-        currentScene = scenes [currentScene].nextScene[5]
+    if (answer === scenes[currentScene].choices[4]) {
+        currentScene = scenes[currentScene].nextScene[4]
     }
+
+    // present scene
+
     presentScene()
 }
-}
 
 
-      
+
+
+
+
 
 
 
@@ -95,7 +102,7 @@ function handelUserChoice(answer) {
 //         selektMessage = 'story2';
 //     else if (selektIndex == 2)
 //         selektMessage = '';
-    
+
 //    document.getElementById("p").innerHTML = colourMessage;
 // }
 // function interFunction() {
